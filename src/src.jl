75d3d1e3058a12caaf2d007adf2cd310f19fa535
zhaoshengEE <jerryli20160808@gmail.com>
2020-12-12 16:10:12 -0800

    Finalize source code

--- src/src.jl
+++ src/src.jl
@@ -1,27 +1,30 @@
 using DataFrames, CSV # For loading data
-using Plots
+using Plots # For plotting the total error in training process
 using Distributions # For generating weights
 
-argA = 0
-argB = 1
-bias = 1
+argA = 0 # An argument value in binary
+argB = 1 # Another arugment value in binary
+bias = 1 # The bias term is used to help the neural network fit best for the input data
 
 inputNum = 5  # Include bias term
 hiddenNum = 4 # Include bias term
 outputNum = 1
-momentum = 0.9
+momentum = 0.9 # A term used to increase the neural network's tranining speed
 learningRate = 0.2
 
+###  Activation Function (Sigmoid)  ###
 function sigmoid(x::Float64)
     result = (argB - argA) / (1 + exp(-x)) + argA
     return result
 end
 
+###  Activation Function (ReLU)  ###
 function ReLu(x::Float64)
     result = max(0.0, x)
     return result
 end
 
+###  Training Function  ###
 function train(input::Array{Float64,1}, expectedOutput::Float64)
     trainOutput = outputFor(input) # forward propagation
     updateWeight(trainOutput, expectedOutput) # backpropagation
@@ -29,6 +32,7 @@ function train(input::Array{Float64,1}, expectedOutput::Float64)
     return errorRate
 end
 
+###  Forward Propagation Function  ###
 function outputFor(input::Array{Float64,1})
     for i = 1:inputNum - 1
         inputNeuron[i] = input[i]
@@ -51,6 +55,7 @@ function outputFor(input::Array{Float64,1})
     return outputNeuron[1]
 end
 
+###  Backpropagation Function  ###
 function updateWeight(trainOutput::Float64, expectedOutput::Float64)
     deltaOutput[1] = trainOutput * (1 - trainOutput) * (expectedOutput - trainOutput)
 
@@ -74,6 +79,7 @@ function updateWeight(trainOutput::Float64, expectedOutput::Float64)
     end
 end
 
+###  Initilize Neurons and Weights  ###
 inputNeuron = zeros(inputNum)
 hiddenNeuron = zeros(hiddenNum)
 outputNeuron = zeros(outputNum)
@@ -81,6 +87,7 @@ outputNeuron = zeros(outputNum)
 inputWeights = rand(Uniform(-0.5, 0.5), (inputNum, hiddenNum - 1))
 hiddenWeights = rand(Uniform(-0.5, 0.5), (hiddenNum, outputNum))
 
+### Error Signal for Neurons and Weights (Used in Backpropagation)  ###
 inputWeightsDelta = zeros(inputNum, hiddenNum - 1)
 hiddenWeightsDelta = zeros(hiddenNum, outputNum)
 
@@ -89,8 +96,9 @@ deltaOutput = zeros(outputNum)
 
 epoch = 0
 errorRate = 0.0
-acceptError = 0.05
+acceptError = 0.05 # The threshold value of error rate
 
+###  Read Data from the CSV File  ###
 df = DataFrame(CSV.File("data/germandata.csv"))
 head(df)
 
@@ -100,8 +108,9 @@ inputCTrain = df[1:600,3]  # Property
 inputDTrain = df[1:600,4]  # Telephone
 expectedOutput = df[1:600,5] # Expected training output
 
-epochVector = zeros(0)
-errorRateVector = zeros(0)
+
+epochVector = zeros(0) # Store epoch number for plotting
+errorRateVector = zeros(0) # Store error rate in each epoch for plotting
 
 ###  Training Process  ###
 
@@ -118,6 +127,7 @@ while(epoch == 0 || errorRate > acceptError)
     epoch += 1
 end
 
+### Plot the Training Process  ###
 plot(epochVector, errorRateVector, title = "Total Squared Error in Training Process", lw = 3, legend = false)
 xlabel!("Epoch")
 ylabel!("Error")
@@ -136,11 +146,14 @@ for index in 1:400
     testOutput = outputFor(testInput)
     realOutput = convert(Float64, expectedOutputTest[index])
 
+    # The trail can be considered as successful only if the testing value is 
+    # within the accepted error range
     if testOutput > realOutput - 0.05 && testOutput < realOutput + 0.05
         count += 1
     end
 
 end
 
+### Calculate the Accuracy  ###
 accuracy = (count / 400) * 100
 println("The accuracy of this trained neural netowrk is $accuracy%")
